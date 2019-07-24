3d297c357b266f0d71a59bd9fa911075e554cae7
HJr Dev <astronomi@gmail.com>
2019-07-24 18:57:30 -0300

    created component PageNavigation

---
+++ src/components/PageNavigation/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+
+const PageNavigation = () => {
+  return (
+    <div className="nav-link-container"> 
+  <a href="#" className="nav-link">Prev</a>
+  <a href="#" className="nav-link">Next</a>
+</div>
+  )
+}
+
+export default PageNavigation;
